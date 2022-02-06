74d37878813c86ceed1a1aea98051ff3c79dccdf
juliocabrera820 <juliocabrera820@gmail.com>
2022-02-06 13:03:17 -0600

    add method to parse feed

--- app/services/rss_service.rb
+++ app/services/rss_service.rb
@@ -5,14 +5,7 @@ class RssService
     URI.open(url)
   end
 
-  def self.collect(news, category_id)
-    feed = RSS::Parser.parse(news)
-    #puts "Title: #{feed.channel.title}"
-    feed.items.each do |item|
-      puts item
-      new_item = New.new(title: item.title, description: item.description,
-                         published: item.pubDate, url: item.link, category_id: category_id)
-      new_item.save!
-    end
+  def self.parse_feed(feed)
+    RSS::Parser.parse(feed)
   end
 end
