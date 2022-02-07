4a1c58aa74660b46f04c6b756382e58b3ee9a7be
juliocabrera820 <juliocabrera820@gmail.com>
2022-02-07 14:10:27 -0600

    add serializer to format response

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,9 +1,15 @@
 module Api
   module V1
     class NewsController < ApplicationController
+      before_action :new, only: [:show]
+
       def index
         news = New.where(feed_id: params[:id])
-        render json: news, status: :ok
+        render json: news, each_serializer: NewSerializer, status: :ok
+      end
+
+      def show
+        render json: new, each_serializer: NewSerializer, status: :ok
       end
 
       def search
@@ -14,6 +20,12 @@ module Api
       def sort_by
         news = NewsQuery.sort_by_field(params[:sort])
         render json: news, status: :ok
+      end
+
+      private
+
+      def new
+        New.find(params[:id])
       end
     end
   end
